8ddd391bad795335f55d55f9e70f3224f1fd785f
Henri Hänninen <henri.j.hanninen@gmail.com>
2019-04-04 16:15:13 +0300

    first commit for new structure

---
+++ src/nlodis_config.cpp
@@ -0,0 +1,16 @@
+/*
+ * LCPT NLO DIS fitter
+ * Henri Hänninen <henri.j.hanninen@student.jyu.fi>, 2017-
+ */
+
+#include "nlodis_config.hpp"
+
+
+// Default configs
+namespace nlodis_config
+{
+    int CUBA_MAXEVAL;
+    double CUBA_EPSREL;
+    bool VERBOSE = false;
+    bool PRINTDATA = false;
+}
---
+++ src/nlodis_config.hpp
@@ -0,0 +1,20 @@
+/*
+ * LCPT NLO DIS fitter
+ * Henri Hänninen <henri.j.hanninen@student.jyu.fi>, 2017-
+ */
+
+// Configs
+
+#ifndef _NLODIS_CONFIG_HPP
+#define _NLODIS_CONFIG_HPP
+
+
+namespace nlodis_config
+{
+    extern int      CUBA_MAXEVAL;
+    extern double   CUBA_EPSREL;
+    extern bool     VERBOSE;
+    extern bool     PRINTDATA;
+}
+
+#endif
