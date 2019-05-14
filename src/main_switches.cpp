77541254d6ab9bf807dfe3f5c297ec0287f8ef7c
Henri Hänninen <henri.j.hanninen@gmail.com>
2019-05-14 14:30:48 +0300

    Update BK evolution prescription support and resumBK fixed coupling support.

--- src/main_switches.cpp
+++ src/main_switches.cpp
@@ -125,18 +125,20 @@ int main( int argc, char* argv[] )
     string_bk = string(argv [2]);
     if (string(argv [2]) == "resumbk"){
             config::LO_BK = false;  // Solve LO BK with running coupling, overrides RESUM settings
+            config::RESUM_DLOG = true; // Resum doulbe logs
+            config::RESUM_SINGLE_LOG = true; // Resum single logs
+            config::KSUB = 0.65;  // Optimal value for K_sub
+            config::NO_K2 = true;  // Do not include numerically demanding full NLO part
     }else if (string(argv [2]) == "kcbk"){
-            cout << endl << "KCBK (old) unsupported at this time!" << endl << endl;
-            exit(1);
             config::LO_BK = true;  // Solve (kinematic / delay) LO BK with running coupling, overrides RESUM settings
             config::EULER_METHOD            = true;        // Kinematical constraint requires this
-            config::KINEMATICAL_CONSTRAINT  = true;
-            config::DE_SOLVER_STEP = 0.08; //0.02; // Euler method requires smaller step than RungeKutta!
+            config::KINEMATICAL_CONSTRAINT  = config::KC_BEUF_K_PLUS;
+            config::DE_SOLVER_STEP = 0.05; //0.02; // Euler method requires smaller step than RungeKutta!
     }else if (string(argv [2]) == "trbk"){  // Target Rapidity BK
             config::LO_BK = true;  // Solve (kinematic / delay) LO BK with running coupling, overrides RESUM settings
             config::EULER_METHOD            = true;        // Kinematical constraint requires this
-            config::KINEMATICAL_CONSTRAINT  = true;
-            config::DE_SOLVER_STEP = 0.08; //0.02; // Euler method requires smaller step than RungeKutta!
+            config::KINEMATICAL_CONSTRAINT  = config::KC_EDMOND_K_MINUS;
+            config::DE_SOLVER_STEP = 0.05; //0.02; // Euler method requires smaller step than RungeKutta!
     }else if (string(argv [2]) == "lobk"){
             config::LO_BK = true;
     } else {cout << helpstring << endl; return -1;}
@@ -151,11 +153,8 @@ int main( int argc, char* argv[] )
             config::RESUM_RC = config::RESUM_RC_GUILLAUME;
             nlodis_config::RC_DIS = nlodis_config::DIS_RC_GUILLAUME;
     } else if (string(argv [3]) == "fixedrc"){
-            if (config::LO_BK == false){
-                cout << "There is no fixed coupling option for RESUM BK" << endl;
-                exit(1); }
             config::RC_LO = config::FIXED_LO;
-            // config::RESUM_RC = config::RESUM_RC_FIXED; // no such option
+            config::RESUM_RC = config::RESUM_RC_FIXED;
             nlodis_config::RC_DIS = nlodis_config::DIS_RC_FIXED;
     } else {cout << helpstring << endl; return -1;}
 
