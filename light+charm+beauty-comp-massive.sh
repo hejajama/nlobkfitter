0a9471f898530adee7704a0374ba31de0de51ff4
Henri Hänninen <henri.j.hanninen@gmail.com>
2022-06-16 09:40:08 +0300

    Add run files for light+c and l+c+b hera comparisons

---
+++ light+charm+beauty-comp-massive.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# light-q-par x001
+    # pdrc
+./massivescan unlpcb resumbk pdrc z2imp unb 75.5 292 1.24 1.0 0.01 1 1 0 0 42.4552 | tee ./out-mass/mq-heratot-l+cb-lightq-urpi-x001.dat
+./massivescan unlpcb kcbk pdrc z2imp unb 75.3 364 1.25 1.0 0.01 1 1 0 0 47.277 | tee ./out-mass/mq-heratot-l+cb-lightq-ukpi-x001.dat
+./massivescan unlpcb trbk pdrc z2imp unb 72.9 305 1.19 1.0 0.01 1 1 0 0 42.2575 | tee ./out-mass/mq-heratot-l+cb-lightq-utpi-x001.dat
+
+#     #balsdrc
+./massivescan unlpcb resumbk balsdrc z2imp unb 79.6 118 1.81 1.0 0.01 1 1 0 0 29.1123 | tee ./out-mass/mq-heratot-l+cb-lightq-ursi-x001.dat
+./massivescan unlpcb kcbk balsdrc z2imp unb 72.0 181 1.55 1.0 0.01 1 1 0 0 31.9494 | tee ./out-mass/mq-heratot-l+cb-lightq-uksi-x001.dat
+./massivescan unlpcb trbk balsdrc z2imp unb 53.7 219 1.59 1.0 0.01 1 1 0 0 43.272 | tee ./out-mass/mq-heratot-l+cb-lightq-utsi-x001.dat
+
+# # light-q-par x1
+#     # pdrc
+./massivescan unlpcb resumbk pdrc z2imp unb 79.4 589 1.92 1.0 1.0 1 1 0 0 68.5359 | tee ./out-mass/mq-heratot-l+cb-lightq-urpi-x1.dat
+./massivescan unlpcb kcbk pdrc z2imp unb 66.4 583 1.47 1.0 1.0 1 1 0 0 69.6422 | tee ./out-mass/mq-heratot-l+cb-lightq-ukpi-x1.dat
+./massivescan unlpcb trbk pdrc z2imp unb 73.1 693 1.86 1.0 1.0 1 1 0 0 76.6397 | tee ./out-mass/mq-heratot-l+cb-lightq-utpi-x1.dat
+
+    #balsdrc
+./massivescan unlpcb resumbk balsdrc z2imp unb 56.6 156 9.35 1.0 1.0 1 1 0 0 36.6534 | tee ./out-mass/mq-heratot-l+cb-lightq-ursi-x1.dat
+./massivescan unlpcb kcbk balsdrc z2imp unb 104.0 206 7.70 1.0 1.0 1 1 0 0 31.0518 | tee ./out-mass/mq-heratot-l+cb-lightq-uksi-x1.dat
+./massivescan unlpcb trbk balsdrc z2imp unb 69.0 553 8.35 1.0 1.0 1 1 0 0 75.1573 | tee ./out-mass/mq-heratot-l+cb-lightq-utsi-x1.dat
+
+# hera x001
+    # pdrc
+./massivescan unlpcb resumbk pdrc z2imp unb 96.4 153 0.98 1.0 0.01 1 1 0 0 19.6678 | tee ./out-mass/mq-heratot-l+cb-hera-urpi-x001.dat
+./massivescan unlpcb kcbk pdrc z2imp unb 83.3 218 0.98 1.0 0.01 1 1 0 0 25.0109 | tee ./out-mass/mq-heratot-l+cb-hera-ukpi-x001.dat
+./massivescan unlpcb trbk pdrc z2imp unb 91.7 114 0.90 1.0 0.01 1 1 0 0 15.8905 | tee ./out-mass/mq-heratot-l+cb-hera-utpi-x001.dat
+
+#    # balsdrc
+./massivescan unlpcb resumbk balsdrc z2imp unb 95.0 70 1.24 1.0 0.01 1 1 0 0 20.1683 | tee ./out-mass/mq-heratot-l+cb-hera-ursi-x001.dat
+./massivescan unlpcb kcbk balsdrc z2imp unb 90.5 131 1.21 1.0 0.01 1 1 0 0 22.2806 | tee ./out-mass/mq-heratot-l+cb-hera-uksi-x001.dat
+./massivescan unlpcb trbk balsdrc z2imp unb 67.8 154 1.13 1.0 0.01 1 1 0 0 26.7847 | tee ./out-mass/mq-heratot-l+cb-hera-utsi-x001.dat
+
+
+# # hera-bothx1
+#     # pdrc
+./massivescan unlpcb resumbk pdrc z2imp unb 72.1 417 1.37 1.0 1.0 1 1 0 0 50.5531 | tee ./out-mass/mq-heratot-l+cb-hera-urpi-x1.dat
+./massivescan unlpcb kcbk pdrc z2imp unb 68.0 410 1.21 1.0 1.0 1 1 0 0 47.2349 | tee ./out-mass/mq-heratot-l+cb-hera-ukpi-x1.dat
+./massivescan unlpcb trbk pdrc z2imp unb 82.0 469 1.44 1.0 1.0 1 1 0 0 50.7895 | tee ./out-mass/mq-heratot-l+cb-hera-utpi-x1.dat
+
+#     # balsdrc
+./massivescan unlpcb resumbk balsdrc z2imp unb 53.0 97 1.56 1.0 1.0 1 1 0 0 25.9394 | tee ./out-mass/mq-heratot-l+cb-hera-ursi-x1.dat
+./massivescan unlpcb kcbk balsdrc z2imp unb 111.4 131 1.94 1.0 1.0 1 1 0 0 21.9128 | tee ./out-mass/mq-heratot-l+cb-hera-uksi-x1.dat
+./massivescan unlpcb trbk balsdrc z2imp unb 64.5 221 6.37 1.0 1.0 1 1 0 0 36.3206 | tee ./out-mass/mq-heratot-l+cb-hera-utsi-x1.dat
+
