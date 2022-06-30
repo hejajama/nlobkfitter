1bc6722070d9158ff809fbb7f5a787aa8c639527
Henri Hänninen <47873564+hhannine@users.noreply.github.com>
2022-06-30 14:25:35 +0300

    DDIS integrand declarations

--- src/ddis.cpp
+++ src/ddis.cpp
@@ -1,19 +1,51 @@
-#include <cmath>
-#include <string>
-#include <vector>
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-//#include <chrono>
 #include <gsl/gsl_sys.h>
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_sf_bessel.h>
 #include <gsl/gsl_sf_log.h>
 #include <gsl/gsl_sf_exp.h>
 
-#include <amplitudelib/amplitudelib.hpp>
-
 #include "nlodis_config.hpp"
+#include "nlodissigmar_massiveq.hpp"
 #include "nlodissigmar.hpp"
-#include "ddis.hpp"
+
+
+double I_ddis_lo_qqbar_L(double Q, double beta, double z, double x01sq, double conj_x01sq) {
+    double res;
+
+}
+
+double I_ddis_lo_qqbar_T(double Q, double beta, double z, double x01sq, double conj_x01sq) {
+    double res;
+    
+}
+
+double I_ddis_nlo_qqbarg_T_largeM(double Q, double z, double x01sq) {
+    double res;
+
+}
+
+double I_ddis_nlo_qqbarg_T_largeQsq(double Q, double beta, double z, double ksq, double x01sq, double conj_x01sq) {
+    double res;
+
+}
+
+double I_ddis_nlo_qqbarg_L(double Q, double beta, double t, double z0, double z1, double z2, double x01sq, double x02sq, double x21sq, double conj_x01sq, double conj_x02sq, double conj_x21sq) {
+    double res;
+
+}
+
+double I_ddis_nlo_qqbarg_L_D3(double Q, double beta, double z0, double z1, double z2, double x01sq, double x02sq, double x21sq, double conj_x01sq, double conj_x02sq, double conj_x21sq) {
+    double res;
+
+}
+
+double I_ddis_nlo_qqbarg_T(double Q, double beta, double t, double z0, double z1, double z2, double x01sq, double x02sq, double x21sq, double conj_x01sq, double conj_x02sq, double conj_x21sq) {
+    double res;
+
+}
+
+double I_ddis_nlo_qqbarg_T_D3(double Q, double beta, double z0, double z1, double z2, double x01sq, double x02sq, double x21sq, double conj_x01sq, double conj_x02sq, double conj_x21sq) {
+    double res;
+
+}
 
