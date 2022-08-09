0eec165f9eb2ab0547ef5fa617be6a722be96c90
Henri Hänninen <47873564+hhannine@users.noreply.github.com>
2022-08-09 16:18:08 +0300

    DDIS F^D integrators cont.

--- src/ddis.cpp
+++ src/ddis.cpp
@@ -9,15 +9,30 @@
 #include "nlodissigmar.hpp"
 
 
-double I_ddis_lo_qqbar_L(double Q, double beta, double z, double x01sq, double conj_x01sq) {
+double I_ddis_lo_qqbar_L(double Q, double beta, double z, double x01, double conj_x01) {
     double res;
-
+    double fac = std::pow(z*(1-z),3.0)*Sq(Q);
+    double z1z_root = std::sqrt(z*(1-z));
+    double Mx = Q * std::sqrt((1-beta)/beta);
+    double j0inner = z1z_root * Mx;
+    double k0inner = z1z_root * Q;
+    double Phi_0 = x01 * gsl_sf_bessel_J0(j0inner*x01) * gsl_sf_bessel_K0(k0inner*x01);
+    double conj_Phi_0 = conj_x01 * gsl_sf_bessel_J0(j0inner*conj_x01) * gsl_sf_bessel_K0(k0inner*conj_x01);
+    res = Phi_0 * conj_Phi_0;
     return res;
 }
 
-double I_ddis_lo_qqbar_T(double Q, double beta, double z, double x01sq, double conj_x01sq) {
+double I_ddis_lo_qqbar_T(double Q, double beta, double z, double x01, double conj_x01) {
     double res;
-    
+    double z1z = z * (1-z);
+    double fac = Sq(z1z * Q) * (Sq(z) + Sq(1-z));
+    double z1z_root = std::sqrt(z1z);
+    double Mx = Q * std::sqrt((1-beta)/beta);
+    double j1inner = z1z_root * Mx;
+    double k1inner = z1z_root * Q;
+    double Phi_1 = x01 * gsl_sf_bessel_J1(j1inner*x01) * gsl_sf_bessel_K1(k1inner*x01);
+    double conj_Phi_1 = conj_x01 * gsl_sf_bessel_J1(j1inner*conj_x01) * gsl_sf_bessel_K1(k1inner*conj_x01);
+    res = Phi_1 * conj_Phi_1;
     return res;
 }
 
--- src/nlodissigmar.cpp
+++ src/nlodissigmar.cpp
@@ -3139,7 +3139,7 @@ int integrand_ddis_lo_qqbar_L(const int *ndim, const double x[], const int *ncom
     double SKernel_conj = 1.0 - Optr->Sr(conj_x01,Xrpdty_lo_bar);
     double res;
 
-    res = SKernel*SKernel_conj*(I_ddis_lo_qqbar_L(Q,beta,z1,x01sq,conj_x01sq))*x01*conj_x01*alphfac;
+    res = SKernel*SKernel_conj*(I_ddis_lo_qqbar_L(Q,beta,z1,x01,conj_x01))*x01*conj_x01*alphfac;
     if(gsl_finite(res)==1){
         *f=res;
     }else{
@@ -3164,6 +3164,35 @@ double ComputeSigmaR::diff_lo_xpom_FL(double Q, double xpom, double beta){
 
 /// --- TTT --- LO --- TTT ---------------------
 
+int integrand_ddis_lo_qqbar_T(const int *ndim, const double x[], const int *ncomp, double *f, void *userdata) {
+    Userdata *dataptr = (Userdata*)userdata;
+    double Q=dataptr->Q;
+    double xpom=dataptr->xpom;
+    double beta=dataptr->beta;
+    ComputeSigmaR *Optr = dataptr->ComputerPtr;
+    double z1=x[0];
+    double x01=nlodis_config::MAXR*x[1];
+    double conj_x01=nlodis_config::MAXR*x[2];
+    double x01sq=Sq(x01);
+    double conj_x01sq=Sq(conj_x01);
+
+    double alphabar=Optr->Alphabar(x01sq);
+    double alphfac=alphabar*CF/Nc;
+    double Xrpdty_lo = Optr->Xrpdty_LO(xpom, Sq(Q), x01sq); // TODO check x val
+    double Xrpdty_lo_bar = Optr->Xrpdty_LO(xpom, Sq(Q), conj_x01sq); // TODO check x val
+    double SKernel = 1.0 - Optr->Sr(x01,Xrpdty_lo);
+    double SKernel_conj = 1.0 - Optr->Sr(conj_x01,Xrpdty_lo_bar);
+    double res;
+
+    res = SKernel*SKernel_conj*(I_ddis_lo_qqbar_T(Q,beta,z1,x01,conj_x01))*x01*conj_x01*alphfac;
+    if(gsl_finite(res)==1){
+        *f=res;
+    }else{
+        *f=0;
+    }
+    return 0;
+}
+
 double ComputeSigmaR::diff_lo_xpom_FT(double Q, double xpom, double beta){
     double integral, error, prob;
     const int ndim=3; // z + r + rbar
@@ -3181,6 +3210,40 @@ double ComputeSigmaR::diff_lo_xpom_FT(double Q, double xpom, double beta){
 
 
 /// --- TTT --- APPROX LIMIT LARGE M, LARGE Q --- TTT ---------------------
+
+int integrand_ddis_nlo_qqbarg_T_largeM(const int *ndim, const double x[], const int *ncomp, double *f, void *userdata) {
+    Userdata *dataptr = (Userdata*)userdata;
+    double Q=dataptr->Q;
+    double xpom=dataptr->xpom;
+    double beta=dataptr->beta;
+    ComputeSigmaR *Optr = dataptr->ComputerPtr;
+    double z1=x[0];
+    double x01=nlodis_config::MAXR*x[1];
+    double x02=nlodis_config::MAXR*x[2];
+    double phix0102=2.0*M_PI*x[3];
+    double x01sq=Sq(x01);
+    double x02sq=Sq(x02);
+    double x21sq=x01sq+x02sq-2.0*sqrt(x01sq*x02sq)*cos(phix0102);
+    double x21 = std::sqrt(x21sq);
+
+    double alphabar=Optr->Alphabar(x01sq);
+    double alphfac=alphabar*CF/Nc;
+    double Xrpdty_lo = Optr->Xrpdty_LO(xpom, Sq(Q), x01sq); // TODO check x val
+    double N01 = 1.0 - Optr->Sr(x01,Xrpdty_lo);
+    double N02 = 1.0 - Optr->Sr(x02,Xrpdty_lo);
+    double N21 = 1.0 - Optr->Sr(x21,Xrpdty_lo);
+    double dipole_kernel = Sq(N02 + N21 - N01 - N02*N21);
+    double bk_kernel = x01sq / (x02sq * x21sq)
+    double res;
+
+    res = bk_kernel*dipole_kernel*(I_ddis_nlo_qqbarg_T_largeM(Q,beta,z1,x01))*x01*x02*alphfac;
+    if(gsl_finite(res)==1){
+        *f=res;
+    }else{
+        *f=0;
+    }
+    return 0;
+}
 
 double ComputeSigmaR::diff_nlo_xpom_FT_qqbarg_largeM(double Q, double xpom, double beta){
     double integral, error, prob;
